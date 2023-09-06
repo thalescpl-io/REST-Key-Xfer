21141facc209eab695af89b9d7c7df72bce7dfea
RickT256 <Rick.Robinson@ThalesGroup.com>
2023-09-06 18:05:06 +0000

    small adjustment to linux script

--- k-rest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Script for executing kk-rest.by on Linux
-#
-python3 k-rest.py \
--srcHost gklm.test256.io \
--srcUser SKLMAdmin -srcPass Thales_4567 \
--dstHost cm-kirk.test256.io \
--dstUser rest_alice -dstPass Thales234!
