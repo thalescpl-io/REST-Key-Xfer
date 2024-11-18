2b1a8485cdeb1d6721bf4f97e935acd58d2aa5ec
Rick Robinson <Rick.Robinson@ThalesGroup.com>
2024-11-18 16:54:03 -0500

    Fresh update of batch file.
    Nothing crazy.  Just keeping the batch file up to date.

--- k-rest.bat
+++ k-rest.bat
@@ -9,5 +9,5 @@ REM Please see README file at https://github.com/thalescpl-io/REST-Key-Xfer for 
 REM py k-rest.py -srcHost xklm-22.test256.io -srcUser sklmrick -srcPass Thales_4567 -dstHost cm-kirk.test256.io -dstUser rest_alice -dstPass Thales234! -listSrcClients -listOnly SOURCE -resolveSrcClientOwnership
 REM py k-rest.py -srcHost 192.168.1.180 -srcUser sklmrick -srcPass Thales_4567 -dstHost 192.168.1.190 -dstUser rest_alice -dstPass Thales234! -listSrcClients -listOnly SOURCE -resolveSrcClientOwnership -includeSecrets
 
-py k-rest.py -srcHost xklm-22.test256.io -srcUser sklmrick -srcPass Thales_4567 -dstHost cm-kirk.test256.io -dstUser rest_alice -dstPass Thales234! -srcClientName KMIPCLIENT5 -resolveSrcClientOwnership -listOnly BOTH
-REM -listSrcClients -listOnly SOURCE -srcClientName  -resolveSrcClientOwnership
+REM py k-rest.py -srcHost xklm-22.test256.io -srcUser sklmrick -srcPass Thales_4567 -dstHost cm-kirk.test256.io -dstUser rest_alice -dstPass Thales234! -srcClientName KMIPCLIENT5 -resolveSrcClientOwnership -includeSecrets
+py k-rest.py -srcHost xklm-22.test256.io -srcUser sklmrick -srcPass Thales_4567 -dstHost cm-kirk.test256.io -dstUser rest_alice -dstPass Thales234! -includeSecrets -listOnly SOURCE -listSrcClients -srcClientName KMIPCLIENT5
